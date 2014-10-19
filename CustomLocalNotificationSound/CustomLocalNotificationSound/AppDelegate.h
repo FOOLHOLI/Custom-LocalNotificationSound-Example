663a12d6925d2f6a067a5a4696a28b8144c2af2b
iOSBeginner <jenhausu@icloud.com>
2014-10-19 16:54:05 +0800

    initialization

---
+++ CustomLocalNotificationSound/CustomLocalNotificationSound/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomLocalNotificationSound
+//
+//  Created by 蘇健豪1 on 2014/10/19.
+//  Copyright (c) 2014年 蘇健豪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
